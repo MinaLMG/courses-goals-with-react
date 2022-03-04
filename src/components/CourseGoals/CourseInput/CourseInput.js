7cbae0f0faf372646b2cbeffcac32722527122f3
minaLMG <beandro6@gmail.com>
2022-03-04 03:31:24 +0200

    added some styling for label and textInput while typing and submitting

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-
-  const goalInputChangeHandler = event => {
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isVAlid, setIsVAlid] = useState(true);
+  const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim() != "") {
+      setIsVAlid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if (enteredValue.trim() == "") {
+      setIsVAlid(false);
+    } else {
+      props.onAddGoal(enteredValue);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: isVAlid ? "black" : "red" }}>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            borderColor: isVAlid ? "black" : "red",
+            backgroundColor: isVAlid ? "transparent" : "salmon",
+          }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
